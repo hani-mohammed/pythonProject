535a52f3ace34f9503d4ff489e02d2e3a1608012
hani-mohammed <hani.mohammed@sait.ca>
2025-08-11 12:47:26 -0600

    Added user option

--- GPA_system.py
+++ GPA_system.py
@@ -1,20 +1,24 @@
 students_data = {}
+option = True
+
 print("Welcome to the students data software.")
+while option:
+    gpa_data = [] #list
+    student = input("What is the student name?\n")
+    for i in range(4):
+        gpaText = input("What is the gpa for the course " + str(i+1)+"\n")
+        gpa = float(gpaText)
+        gpa_data.append(gpa)
 
-gpa_data = [] #list
-student = input("What is the student name?\n")
-for i in range(4):
-    gpaText = input("What is the gpa for the course " + str(i))
-    gpa = float(gpaText)
-    gpa_data.append(gpa)
+    sum = 0
+    for gpa in gpa_data:
+        sum += gpa
 
-sum = 0
-for gpa in gpa_data:
-    sum += gpa
+    avg_gpa = sum/4
 
-avg_gpa = sum/4
-
-students_data[student] = avg_gpa
+    students_data[student] = avg_gpa
+    user_input = input("Would you like to add more students data?y/n\n")
+    option = (user_input == 'y')
 
 print(students_data)
 
