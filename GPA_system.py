4c1fbbf43696542a41d08ee8dd341d869ef1ceea
hani-mohammed <hani.mohammed@sait.ca>
2025-08-11 12:36:39 -0600

    initial implementation of student app

--- GPA_system.py
+++ GPA_system.py
@@ -1,2 +1,21 @@
-print("Welcome to the student GPA management software.")
-print("What is your name?")
+students_data = {}
+print("Welcome to the students data software.")
+
+gpa_data = [] #list
+student = input("What is the student name?\n")
+for i in range(4):
+    gpaText = input("What is the gpa for the course " + str(i))
+    gpa = float(gpaText)
+    gpa_data.append(gpa)
+
+sum = 0
+for gpa in gpa_data:
+    sum += gpa
+
+avg_gpa = sum/4
+
+students_data[student] = avg_gpa
+
+print(students_data)
+
+
