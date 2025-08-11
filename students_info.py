40fe1dfbb4b7ad98e70e8cc4269573f73885a350
hani-mohammed <hani.mohammed@sait.ca>
2025-08-11 13:58:06 -0600

    added remove function

--- students_info.py
+++ students_info.py
@@ -1,12 +1,25 @@
 from util import *
 
 students ={
-    #1234: [1234, "Sam", 3.4, 3],
-    #3321: [3321, "Sara", 4.0, 1]
+    123: [123, "Sam", 3.4,3],
+    234: [234, "Sara", 3.5,3],
+    435: [435, "John", 3.2,1],
+    563: [563, "Tom", 3.1,2],
 }
 continue_option = True
 while continue_option:
-    show_menu()
+    user_option = show_menu()
+    match user_option:
+        case 1:
+            run_search(students)
+        case 2:
+            run_edit(students)
+        case 3:
+            run_add(students)
+        case 4: 
+            run_remove(students)
+        case 5:
+            run_print(students)
     user_input = input("Would you like to do something else?y/n\n")
     if user_input.lower() =='n':
         continue_option = False
--- util.py
+++ util.py
@@ -4,3 +4,22 @@ def show_menu():
     print("-Edit a student info? 2")
     print("-Add a student? 3")
     print("-Remove a student? 4")
+    print("-print the students list? 5")
+    return int(input(""))
+
+def run_search(students):
+    print("Searching")
+
+def run_edit(students):
+    print("Editing")
+
+def run_add(students):
+    print("Adding")
+
+def run_remove(students):
+    id = int(input("Please Enter user id:\n"))
+    del students[id]
+    return 
+
+def run_print(students):
+    print(students)
