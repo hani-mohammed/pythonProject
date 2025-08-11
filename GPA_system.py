c48449e4d340964b0a3246d2dc72d757a9b1193e
hani-mohammed <hani.mohammed@sait.ca>
2025-08-11 13:15:56 -0600

    Added support to multi students

--- GPA_system.py
+++ GPA_system.py
@@ -1,25 +1,32 @@
 students_data = {}
-option = True
-
+more_students_option = True
 print("Welcome to the students data software.")
-while option:
-    gpa_data = [] #list
-    student = input("What is the student name?\n")
-    for i in range(4):
-        gpaText = input("What is the gpa for the course " + str(i+1)+"\n")
-        gpa = float(gpaText)
-        gpa_data.append(gpa)
-
+while more_students_option:
     sum = 0
-    for gpa in gpa_data:
-        sum += gpa
+    count = 0
+    student = input("What is the student name?\n")
+    more_gpa_option = True
+    while more_gpa_option:
+        gpaText = input("Enter the GPAs. Enter -1 to finish adding gpas.\n")
+        gpa = float(gpaText)
+        if gpa != -1:
+            sum += gpa
+            count +=1
+        else :
+            more_gpa_option = False
 
-    avg_gpa = sum/4
+
+    avg_gpa = sum/count
 
     students_data[student] = avg_gpa
+    print("Student "+ student +"  was added successfully.")
     user_input = input("Would you like to add more students data?y/n\n")
-    option = (user_input == 'y')
-
+    if user_input.lower() == 'y' or user_input.lower() == 'yes':
+        more_students_option = True
+    elif user_input.lower() == 'n' or user_input.lower() == 'no':
+        more_students_option = False
+    else:
+        raise NameError("Please Enter a valid option")
 print(students_data)
 
 
